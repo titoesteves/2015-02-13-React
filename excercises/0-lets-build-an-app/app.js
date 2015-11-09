79faf043619fa9ad287f22a89b73865cc85ef6cf
Tito Esteves <titoesteves@live.com>
2015-11-09 10:34:39 -0500

    hello world react

---
+++ excercises/0-lets-build-an-app/app.js
@@ -0,0 +1,47 @@
+var React = require('react');
+
+var fetchUsers = (cb) => {
+	setTimeout(() => {
+		cb([{name: 'Ryan'}, {name: 'Marc'}, {name: 'Mark'}]);
+	}, 500)
+}
+
+var App = React.createClass({
+	getInitialState () {
+		return {
+			users: [],
+			loaded: false
+		}
+	},
+	componentDidMount () {
+		fetchUsers((users) => {
+			this.setState({ 
+				users,
+				loaded: true
+			 });
+		})
+	},
+
+	deleteUsers (target) {
+		var users = this.state.users;
+		var withoutUsers = users.filter(user => user.name !== target.name);
+		this.setState({users: withoutUsers})
+	},
+	render () {
+		if(!this.state.loaded) {
+			return <div> Loading </div>;
+		}
+		var users = this.state.users.map((user) => {
+			return <li onClick={this.deleteUsers.bind(this, user)}> {user.name} </li>;
+		});
+		return (
+			<div>
+				<h1>Hello</h1>
+				<ul>
+					{users}
+				</ul>
+			</div>
+		);
+	}
+});
+React.render(<App/>, document.body);
